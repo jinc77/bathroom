78eeeb7237418d550aa0fec021d74e7c45ddcbf7
Uijin Cho <98933317+jinc77@users.noreply.github.com>
2024-12-13 09:30:13 -0800

    Update Urinal.java
    javadoc for urinal

--- src/Urinal.java
+++ src/Urinal.java
@@ -1,4 +1,14 @@
+/* Bathroom subclass Urinal that can store information about
+ * the bathroom fixture: what number urinal,
+ * whether it's occupied, how many times it was used,
+ * and who is currently using the urinal.
+ * @author Uijin Cho
+ */
 public class Urinal extends Bathroom {
+
+    /* Constructor to create a Urinal object
+     * @param id           the fixture number
+     */
     public Urinal(String id) {
         super(id);
     }
